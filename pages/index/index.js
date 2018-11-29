71436dbe9dad467f1553d405e6024be436982dd6
swk <swkende@163.com>
2018-11-29 22:58:15 +0800

    推荐页面点击按钮能获取数据完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,71 @@
 const recommend = require("../../utils/recommend.js")
 Page({
   data: {
-    items:''
+    items: '',
+    position: []
   },
   onLoad: function() {
     let that = this;
     recommend.init(that);
-    console.log(this.data.items);
+    let position = [{
+      "price": "2000",
+      "name": "0-2000元",
+      "position_data": []
+    }, {
+      "price": "4000",
+      "name": "2000-4000元",
+      "position_data": []
+    }, {
+      "price": "6000",
+      "name": "4000-6000元",
+      "position_data": []
+    }, {
+      "price": "8000",
+      "name": "6000-8000元",
+      "position_data": []
+    }, {
+      "price": "10000",
+      "name": "8000元以上",
+      "position_data": []
+    }]
+    this.setData({
+      position: position
+    })
+  },
+  onShow() {
+    let items = this.data.items;
+    for (let i = 0; i < items.length; i++) {
+      let data = this.data.position;
+      let price =
+        items[i].board_p +
+        items[i].chassis_p +
+        items[i].cpu_p +
+        items[i].hard_disk_p +
+        items[i].hot_p +
+        items[i].memory_p +
+        items[i].power_p +
+        items[i].video_card_p;
+      if (0 < price && price <= 2000) {
+        data[0].position_data[i] = items[i]
+      } else if (2000 < price && price <= 4000) {
+        data[1].position_data[i] = items[i]
+      } else if (4000 < price && price <= 6000) {
+        data[2].position_data[i] = items[i]
+      } else if (6000 < price && price <= 8000) {
+        data[3].position_data[i] = items[i]
+      } else if (8000 < price) {
+        data[4].position_data[i] = items[i]
+      }
+    }
   },
   clickbutton(e) {
-    console.log(e.currentTarget.dataset.id)
+    let id = e.currentTarget.dataset.id;
+    let position = this.data.position;
+    for (let i = 0; i < position.length; i++) {
+      if (id == position[i].price) {
+        console.log(position[i])
+      }
+    }
+
   }
 })
--- utils/recommend.js
+++ utils/recommend.js
@@ -1,159 +1,61 @@
 var items = [{
-    "title": "2000",
-    "value": [{
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }]
-  },
-  {
-    "title": "4000",
-    "value": [{
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }]
-  },
-  {
-    "title": "6000",
-    "value": [{
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }]
-  },
-  {
-    "title": "8000",
-    "value": [{
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }]
-  },
-  {
-    "title": "10000",
-    "value": [{
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }, {
-      "cpu": "",
-      "board": "",
-      "memory": "",
-      "hot": "",
-      "video_card": "",
-      "hard_disk": "",
-      "chassis": "",
-      "power": ""
-    }]
-  },
-]
+  "type": "Intel",
+  "title": "i5 8400高性能游戏平台",
+  "cpu": "Intel i5 8400",
+  "cpu_p": 1273,
+  "board": "微星B360M-MORTAR",
+  "board_p": 599,
+  "memory": "芝奇DDR4 8G",
+  "memory_p": 369,
+  "hot": "酷冷至尊T400i散热器",
+  "hot_p": 79,
+  "video_card": "华硕雪豹GTX1060 6G",
+  "video_card_p": 2099,
+  "hard_disk": "三星860evo固态 250G",
+  "hard_disk_p": 329,
+  "chassis": "酷冷至尊MasterBox Lite5机箱",
+  "chassis_p": 299,
+  "power": "航嘉jumper450B 450W",
+  "power_p": 269
+}, {
+  "type": "Intel",
+  "title": "i5 8400超高性价比平台",
+  "cpu": "Intel i5 8400",
+  "cpu_p": 1273,
+  "board": "昂达H310C-SD3",
+  "board_p": 339,
+  "memory": "H110/H310集邦DDR3 8G",
+  "memory_p": 119,
+  "hot": "酷冷至尊T400i散热器",
+  "hot_p": 79,
+  "video_card": "迪兰恒进RX580 8G X-Serial战将",
+  "video_card_p": 1369,
+  "hard_disk": "铭瑄终结者固态 240G",
+  "hard_disk_p": 219,
+  "chassis": "TT启航者F1机箱",
+  "chassis_p": 169,
+  "power": "安钛克VP450P 450W",
+  "power_p": 259
+}, {
+  "type": "Intel",
+  "title": "i5 8400超高性价比平台",
+  "cpu": "Intel i5 8400",
+  "cpu_p": 1273,
+  "board": "昂达H310C-SD3",
+  "board_p": 339,
+  "memory": "H110/H310集邦DDR3 8G",
+  "memory_p": 119,
+  "hot": "酷冷至尊T400i散热器",
+  "hot_p": 79,
+  "video_card": "迪兰恒进RX580 8G X-Serial战将",
+  "video_card_p": 1369,
+  "hard_disk": "铭瑄终结者固态 240G",
+  "hard_disk_p": 219,
+  "chassis": "TT启航者F1机箱",
+  "chassis_p": 169,
+  "power": "安钛克VP450P 450W",
+  "power_p": 250
+}]
 
 function init(that) {
   that.setData({
