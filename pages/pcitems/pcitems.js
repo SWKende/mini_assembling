686a6b52e98e41c78c6491fb0632cb2d25e1082a
swk <swkende@163.com>
2018-12-09 20:31:04 +0800

    完成电脑预算列表中数组的导入（处理了null）

--- pages/pcitems/pcitems.js
+++ pages/pcitems/pcitems.js
@@ -8,9 +8,16 @@ Page({
     wx.getStorage({
       key: 'pc_items',
       success(res) {
-        console.log(res.data)
+        let arr = new Array();
         for (let i = 0; i < res.data.length; i++) {
+          if (res.data[i] != null) {
+            arr.push(res.data[i]);
+          }
         }
+        console.log(arr);
+        that.setData({
+          items: arr
+        })
       },
     })
   },
